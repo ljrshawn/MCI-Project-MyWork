a2f6427c3fee98d5dad5ad7dc51d9de94750254d
Shawn Lyu <a1832252@adelaide.edu.au>
2023-06-07 22:24:56 +0930

    edit for deply

--- MyWork/client/src/utils/config.ts
+++ MyWork/client/src/utils/config.ts
@@ -1,4 +1,4 @@
-export const SERVER_ADDRESS = "http://127.0.0.1:3030/api/v1";
+export const SERVER_ADDRESS = "https://mywork-e26p.onrender.com/api/v1";
 export const CLIENT_ADDRESS = "http://127.0.0.1:3000/";
 
 export const REGISTER_CODE = "ONLYTEACHER";
--- MyWork/server/src/utils/email.js
+++ MyWork/server/src/utils/email.js
@@ -14,10 +14,11 @@ module.exports = class Email {
     if (process.env.NODE_ENV === "production") {
       // Sendgrid
       return nodemailer.createTransport({
-        service: "SendGrid",
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
         auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_PASSWORD,
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
         },
       });
     }
